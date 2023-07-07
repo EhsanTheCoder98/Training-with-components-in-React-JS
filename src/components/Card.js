106fdaf9f7a3c8dff80cc43c769acdbda708cdaa
Ethan Fathalipour <www.ehsanfathalipour110@gmail.com>
2023-07-08 02:00:37 +0330

    Counter of each product added!

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import styles from "./Card.module.css"
+import down from "../WallPaper/down.svg"
+import up from "../WallPaper/up.svg"
 
 
 class Card extends Component {
@@ -10,6 +12,11 @@ class Card extends Component {
                 <img src={img}  alt="NodeJS logo" className={styles.image}/>
                 <h3>Price : {price}</h3>
                 <p>This course is for {intention}</p>
+                <div className={styles.counter}>
+                    <img src={up}   alt='up'/>
+                    <span>0</span>
+                    <img src={down} alt='down'/>
+                </div>
             </div>
         );
     }
